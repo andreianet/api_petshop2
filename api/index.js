fcccb5ca39657ec17dd1b201d49b8ae97956d205
andreianet <andreia.netsys@hotmail.com>
2021-01-22 08:40:54 -0300

    subindo api rest - petshop2

---
+++ api/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const config = require('config');
+const NaoEncontrado = require('./erros/NaoEncontrado')
+const CampoInvalido = require('./erros/CampoInvalido')
+const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos')
+const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+const formatosAceitos = require('./Serializador').formatosAceitos
+const SerializadorErro = require('./Serializador').SerializadorErro
+
+app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+    let formatoRequisitado = req.header('Accept')
+
+    if (formatoRequisitado === '*/*') {
+        formatoRequisitado = 'application/json'
+    }
+
+    if(formatosAceitos.indexOf(formatoRequisitado) === -1){
+        res.status(406)
+        res.end();
+        return
+    }
+
+    res.setHeader('Content-Type', formatoRequisitado)
+    next()
+})
+
+const router = require('./rotas/fornecedores')
+app.use('/api/fornecedores', router);
+
+app.use((erro, req, res, next) =>{
+    let status = 500
+
+    if (erro instanceof NaoEncontrado) {
+        status = 404
+    }
+    if (erro instanceof CampoInvalido || erro instanceof DadosNaoFornecidos) {
+        status = 400
+    }
+    if(erro instanceof ValorNaoSuportado){
+        status = 406;
+    }
+    const serializador = new SerializadorErro(
+    res.getHeader('Content-Type')
+    )
+    res.status(status)
+    res.send(
+        serializador.serializar({
+            mensagem: erro.message,
+            id: erro.idErro
+        })
+    )
+})
+
+
+
+
+app.listen(config.get('api.porta'), () => 
+console.log('API Rodando ...'));
---
+++ api/rotas/fornecedores/index.js
@@ -0,0 +1,89 @@
+const router = require('express').Router();
+const TabelaFornecedor = require('./TabelaFornecedor')
+const Fornecedor = require('./Fornecedor');
+const { SerializadorFornecedor } = require('../../Serializador');
+
+
+
+router.get('/', async (req, res) => {
+    const resultados = await TabelaFornecedor.listar()
+    res.status(200)
+    const serializador = new SerializadorFornecedor(
+        res.getHeader('Content-Type')
+    )
+    res.send(
+        serializador.serializar(resultados)
+    )
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const dadosRecebidos = req.body;
+        const fornecedor = new Fornecedor(dadosRecebidos)
+        await fornecedor.criar()
+        res.status(201)
+        const serializador = new SerializadorFornecedor(
+            res.getHeader('Content-Type')
+        )
+        res.send(
+        serializador.serializar(fornecedor)
+    )
+    } catch (erro) {
+        next(erro);
+    }
+})
+
+router.get('/:idFornecedor', async (req, res, next) => {
+    try {
+        const id = req.params.idFornecedor
+        const fornecedor = new Fornecedor({id: id})
+        await fornecedor.carregar()
+        res.status(200)
+        const serializador = new SerializadorFornecedor(
+            res.getHeader('Content-Type'),
+            //pegar os CamposExtras - Serializador
+            ['email', 'dataCriacao', 'dataAtualizacao', 'versao']
+        )
+        res.send(
+            serializador.serializar(fornecedor)
+        
+        )
+        } catch (erro) {
+        next(erro)        
+    }
+})
+
+router.put('/:idFornecedor', async (req, res, next) => {
+    try {
+        const id = req.params.idFornecedor
+        const dadosRecebidos = req.body
+        const dados = Object.assign({}, dadosRecebidos, { id: id })
+        const fornecedor = new Fornecedor(dados)
+        await fornecedor.atualizar()
+        res.status(204)
+        res.end()
+    } catch (erro) {
+        next(erro)        
+    }
+})
+
+router.delete('/:idFornecedor', async (req, res) => {
+    try {
+        const id = req.params.idFornecedor;
+        const fornecedor = new Fornecedor({id:id})
+        await fornecedor.carregar();
+        await fornecedor.remover();
+        res.status(204)
+        res.end()
+    } catch (erro) {
+        res.status(404)
+        res.send(
+            JSON.stringify({
+                mensagem: erro.message
+            })
+        )
+    }
+})
+
+
+module.exports = router
